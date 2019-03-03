cc8c957cbb7e3dc49113f44cff422f5f5cec1382
Neil Weidinger <neilweidingern@gmail.com>
2019-03-03 05:05:07 +0000

    overlay now has KO text

--- content.js
+++ content.js
@@ -20,6 +20,21 @@ chrome.runtime.onConnect.addListener(function(p) {
         overlay.style.height = "100%";
         overlay.style.zIndex = "200000000";
         overlay.style.backgroundColor = "rgb(153,26,4,0.5)";
+
+        var googleFontLink = document.createElement("link");
+        googleFontLink.setAttribute("href", "https://fonts.googleapis.com/css?family=Press+Start+2P");
+        googleFontLink.setAttribute("rel", "stylesheet");
+
+        var ko = document.createElement("h1");
+        var text = document.createTextNode("KO");
+        ko.style.fontFamily = "'Press Start 2P', cursive";
+        ko.style.color = "white";
+        ko.style.textAlign = "center";
+        ko.style.fontSize = "230px";
+
+        document.head.appendChild(googleFontLink);
         document.body.appendChild(overlay);
+        overlay.appendChild(ko);
+        ko.appendChild(text);
     });
 });

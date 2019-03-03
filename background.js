19d909ab82376456e838395da5abe2c11e5db0f1
Neil Weidinger <neilweidingern@gmail.com>
2019-03-03 02:33:47 +0000

    displays scroll total in inches now

--- background.js
+++ background.js
@@ -1,21 +1,24 @@
-var total = 0;
+var totalPixels = 0;
 
 // runs when started
 chrome.runtime.onInstalled.addListener(function() {
-    console.log("Hello world");
-    displayDist("____");
+    console.log("Scroll Fighter Launched");
 });
 
 // listener for when we receive messages from content script
 chrome.runtime.onConnect.addListener(function(port) {
     port.onMessage.addListener(function(msg) {
-        total += Math.abs(msg.data);
-        displayDist(total.toFixed().toString());
-        console.log("received " + msg.data);
+        totalPixels += Math.abs(msg.data);
+        displayOnBadge(pixelToDist(totalPixels).toString());
     });
 });
 
-function displayDist(word) {
+// returns a distance in pixels to inches
+function pixelToDist(pixels) {
+    return (pixels / 227).toFixed() // MacBook Pro is 227 ppi
+}
+
+function displayOnBadge(word) {
     chrome.browserAction.setBadgeText({
 		text:word
 	});
--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 var backgroundPage = chrome.extension.getBackgroundPage();
 
 function updateScrollTotal() {
-    document.querySelector("#miles").innerText = backgroundPage.total;
+    document.querySelector("#miles").innerText = backgroundPage.pixelToDist(backgroundPage.totalPixels) + " inches";
 }
 setInterval(updateScrollTotal, 10);
